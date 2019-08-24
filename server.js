de1fbef10ff0dc5cbd3c9612e759745f620892b2
curtdragon <tealturboser@gmail.com>
2019-08-23 20:18:07 -0500

    Finished all SQL and Knex

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
-const db = require('./data/dbConfig.js');
-
+const AccountsRouter = require("./accounts/AccountRouter");
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", AccountsRouter);
+
+server.get("/", (req,res) => {
+    res.send("Server Operational")
+});
 
 module.exports = server;
